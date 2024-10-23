bec25a05145333e113b01486868422d0d1fd6c18
Bastiasa <leysantqm@gmail.com>
2024-10-23 17:34:13 -0500

    Auto commit 10/23/2024 17:34:12

--- gamemaker/Caviche Town/extensions/ext_android_socket_udp/AndroidSource/Java/a.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.bind(("0.0.0.0", 7890))
-s.sendto(b'cts:holi', ("255.255.255.255", 8888))
